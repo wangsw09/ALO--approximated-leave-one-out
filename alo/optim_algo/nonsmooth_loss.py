2c4bb246fa3238e2940cb46a9574ccd14f7f50a6
Shuaiwen Wang <sw2853@columbia.edu>
2018-01-31 15:45:16 -0500

    nonsmooth loss with smooth regularizers

---
+++ alo/optim_algo/nonsmooth_loss.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numpy.random as npr
+import glmnet_python
+from glmnet import glmnet
+
+def svm(y, X, lam, intercept=False):
+    return None
+
+def ker_svm(y, X, lam, intercept=False):
+    return None
