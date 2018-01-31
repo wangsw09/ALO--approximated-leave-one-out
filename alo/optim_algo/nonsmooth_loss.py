5f715ba885ed51009395686e5e283984fbd80efe
Shuaiwen Wang <sw2853@columbia.edu>
2018-01-31 17:51:19 -0500

    comment out original implement, use sklearn svm for linear case

--- alo/optim_algo/nonsmooth_loss.py
+++ alo/optim_algo/nonsmooth_loss.py
@@ -1,10 +1,37 @@
 import numpy as np
 import numpy.random as npr
-import glmnet_python
-from glmnet import glmnet
+from scipy.linalg import eigh as eig_max
+from sklearn import svm
 
-def svm(y, X, lam, intercept=False):
-    return None
+from .proximal import *
 
-def ker_svm(y, X, lam, intercept=False):
+def svm_linear(y, X, lam, intercept=False, tol=1e-5, max_iter=500):
+    clf = svm.LinearSVC(loss='hinge', C=1.0 / lam, tol=tol, fit_intercept=False)
+    clf.fit(X, y)
+    beta = clf.coef_[0]
+    return beta
+
+# def svm(y, X, lam, intercept=False, tol=1e-5, max_iter=500):
+#     if intercept is False:
+#         n, p = X.shape
+#         XXT = np.dot(X, X.T)
+#         ss = 0.9 * lam / eig_max(XXT, eigvals=(n-1, n-1), eigvals_only=True)[0]
+#         mu0 = np.ones(n)
+#         mu = np.zeros(n)
+#         
+#         iter_count = 0
+#         
+#         while np.amax(np.fabs(mu - mu0)) > tol and iter_count < max_iter:
+#             np.copyto(mu0, mu)
+#             mu = proj_svm(mu - ss / lam * np.dot(XXT, mu) - ss * y, y)
+#             iter_count += 1
+#         
+#         print iter_count
+#         beta = - np.dot(X.T, mu) / lam
+#         return beta
+# 
+#     else:
+#         return None
+# 
+def svm_ker(y, X, lam, intercept=False):
     return None
