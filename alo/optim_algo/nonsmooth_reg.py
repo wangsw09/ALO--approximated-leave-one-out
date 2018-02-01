adeb48700be514d98ecb8ac411ead6a5b9b2da74
Shuaiwen Wang <sw2853@columbia.edu>
2018-02-01 15:26:56 -0500

    fixed some bugs in Lasso glmnet

--- alo/optim_algo/nonsmooth_reg.py
+++ alo/optim_algo/nonsmooth_reg.py
@@ -9,24 +9,41 @@ def glm_lasso(y, X, lam=None, lam_seq=None, intercept=False, family="gaussian"):
     """
     We are going to simply wrap the functions in glmnet.
     """
+    n, p = X.shape
+    m = lam_seq.shape[0]
     if lam is not None:
         if not intercept:
             beta = glmnet(x=X, y=y, family=family, intr=False, alpha=1, nlambda=1,
-                    lambdau=np.array([lam]))['beta'][:, 0]
+                    lambdau=np.array([lam / n]), standardize=Fales)['beta'][:, 0]
             return beta
         else:
             res = glmnet(x=X, y=y, family=family, intr=True, alpha=1, nlambda=1,
-                    lambdau=np.array([lam]))
+                    lambdau=np.array([lam / n]), standardize=False)
             return res['beta'][:, 0], res['a0'][0]
     elif lam_seq is not None:
         if not intercept:
-            beta = glmnet(x=X, y=y, family=family, intr=False, alpha=1, nlambda=1,
-                    lambdau=np.asarray(lam_seq))['beta']
+            beta = np.empty((p, m))
+            lam_seq_sorted = np.sort(lam_seq)[::-1]
+            lam_seq_rank = np.argsort(lam_seq)[::-1]
+            if family == "gaussian":
+                beta[:, lam_seq_rank] = glmnet(x=X, y=y / np.std(y), family=family, intr=False,
+                        alpha=1, nlambda=1, lambdau=np.asarray(lam_seq_sorted) / n,
+                        standardize=False)['beta'] * np.std(y)
+            else:
+                beta[:, lam_seq_rank] = glmnet(x=X, y=y, family=family, intr=False, alpha=1, nlambda=1,
+                        lambdau=np.asarray(lam_seq_sorted) / n, standardize=False)['beta']
             return beta
         else:
-            res = glmnet(x=X, y=y, family=family, intr=True, alpha=1, nlambda=1,
-                    lambdau=np.asarray(lam_seq))
-            return res['beta'], res['a0']
+            beta = np.empty((p, m))
+            beta0 = np.empty(m)
+            lam_seq_sorted = np.sort(lam_seq)[::-1]
+            lam_seq_rank = np.argsort(lam_seq)[::-1]
+
+            res = glmnet(x=X, y=y, family=family, intr=True, alpha=0, nlambda=1,
+                    lambdau=np.asarray(lam_seq_sorted) / n, standardize=False)
+            beta[:, lam_seq_rank] = res["beta"]
+            beta0[lam_seq_rank] = res["a0"]
+            return beta, beta0
  
 def slope(y, X, lam, weights, intercept=False):
     return None
