225579810e18f363acdceedc2d9caafd290df909
Shuaiwen Wang <sw2853@columbia.edu>
2018-02-01 15:27:24 -0500

    add alo and loocv for lasso

--- alo/alo.py
+++ alo/alo.py
@@ -15,18 +15,34 @@ def alo_glm_ridge(y, X, lam_seq=None, intercept=False,
                 family=family)
         U_mat = np.dot(X, beta_mat)
         for i in xrange(m):
-            # print np.exp(U_mat[:, i])
             ld, ldd = glm_loss(y, U_mat[:, i], family=family)
             H = np.dot(X, npla.solve(np.dot(X.T * ldd, X) + lam_seq[i] * np.eye(p), X.T))
-            # print ld, ldd
             y_hat = np.diag(H) / (1.0 - np.diag(H) * ldd) * ld + U_mat[:, i]
             alo_err[i] = np.mean(err_func(y, y_hat))
         return alo_err
     else:
         raise NotImplementedError
 
-def alo_glm_lasso():
-    return None
+def alo_glm_lasso(y, X, lam_seq=None, intercept=False,
+        family="gaussian", err_func=lambda y1, y2: (y1 - y2) ** 2):
+    n, p = X.shape
+    m = lam_seq.shape[0]
+    alo_err = np.empty(m)
+
+    if intercept is False:
+        beta_mat = oa.glm_lasso(y, X, lam_seq=lam_seq, intercept=False,
+                family=family)
+        U_mat = np.dot(X, beta_mat)
+        for i in xrange(m):
+            ld, ldd = glm_loss(y, U_mat[:, i], family=family)
+            XE = X[:, beta_mat[:, i] != 0]
+            k = XE.shape[1]
+            H = np.dot(XE, npla.solve(np.dot(XE.T * ldd, XE), XE.T))
+            y_hat = np.diag(H) / (1.0 - np.diag(H) * ldd) * ld + U_mat[:, i]
+            alo_err[i] = np.mean(err_func(y, y_hat))
+        return alo_err
+    else:
+        raise NotImplementedError
 
 def alo_L_inf():
     return None
--- alo/loocv.py
+++ alo/loocv.py
@@ -25,4 +25,25 @@ def loo_glm_ridge(y, X, lam_seq=None, intercept=False,
     else:
         raise NotImplementedError
 
+def loo_glm_lasso(y, X, lam_seq=None, intercept=False,
+        family="gaussian", err_func=lambda y1, y2: (y1 - y2) ** 2 ):
+    n, p = X.shape
+    m = lam_seq.shape[0]
+    loo_err = np.zeros(m)
+
+    mask = np.ones(n, dtype=np.bool)
+
+    if intercept is False:
+        for i in xrange(n):
+            mask[i] = False
+            beta_mat = oa.glm_lasso(y[mask], X[mask, :], lam_seq=lam_seq, intercept=False,
+                family=family)
+            pred = np.dot(X[i, :], beta_mat)
+            loo_err += err_func(y[i], pred)
+            mask[i] = True
+        return loo_err / n
+
+    else:
+        raise NotImplementedError
+
 
